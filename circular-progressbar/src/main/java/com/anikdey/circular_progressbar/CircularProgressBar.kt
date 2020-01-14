b616c26feb5b822bbe8ebdbc3d4a6ff57f89fb79
Tanvir Ahmod <shoukhin1993@gmail.com>
2020-01-14 17:19:37 +0600

    CircularProgressBar code refactored

--- circular-progressbar/src/main/java/com/anikdey/circular_progressbar/CircularProgressBar.kt
+++ circular-progressbar/src/main/java/com/anikdey/circular_progressbar/CircularProgressBar.kt
@@ -11,11 +11,13 @@ import android.view.animation.Transformation
 /**
  * Created by Anik Dey on 1/7/2020
  */
-class CircularProgressBar : View {
+class CircularProgressBar constructor(context: Context, attrs: AttributeSet?) :
+    View(context, attrs) {
 
-    private lateinit var progressRect: RectF
-    private lateinit var innerRect: RectF
+    private var progressRect: RectF
+    private var innerRect: RectF
     private var progressBarWidth = 10f
+    private var innerGap = 20f + (progressBarWidth / 2)
     private var angle: Float = 0.toFloat()
     private var innerCircleColor: Int = Color.WHITE
     private var progressCircleColor: Int = Color.BLACK
@@ -29,63 +31,68 @@ class CircularProgressBar : View {
     private val textBounds = Rect()
     private var percentageCompletedText = ""
     private var textSize = 50
+    private val paint = Paint()
 
-    constructor(context: Context) : super(context) {
-        init(context, null)
-    }
+    init {
+        paint.isAntiAlias = true
+        innerGap = 20f + (progressBarWidth / 2)
+        progressRect = RectF(
+            progressBarWidth,
+            progressBarWidth,
+            calculatedWidth.toFloat() - progressBarWidth,
+            calculatedHeight.toFloat() - progressBarWidth
+        )
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        init(context, attrs)
-    }
+        innerRect = RectF(
+            progressBarWidth + innerGap,
+            progressBarWidth + innerGap,
+            calculatedWidth.toFloat() - progressBarWidth - innerGap,
+            calculatedHeight.toFloat() - progressBarWidth - innerGap
+        )
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
-        init(context, attrs)
-    }
-
-    constructor(
-        context: Context,
-        attrs: AttributeSet?,
-        defStyleAttr: Int,
-        defStyleRes: Int
-    ) : super(context, attrs, defStyleAttr, defStyleRes) {
-        init(context, attrs)
-    }
-
-
-    private fun init(context: Context, attrs: AttributeSet?) {
-        attrs?.let {attributes->
-            var typedArray = context.obtainStyledAttributes(attributes, R.styleable.CircularProgressBar)
-            textSize = typedArray.getDimensionPixelSize(R.styleable.CircularProgressBar_mtextSize, textSize)
-            startAngel = typedArray.getFloat(R.styleable.CircularProgressBar_mstartAngle, startAngel)
-            progressBarWidth = typedArray.getFloat(R.styleable.CircularProgressBar_progressBarWidth, progressBarWidth)
-            percentCompleted = typedArray.getFloat(R.styleable.CircularProgressBar_mpercentCompleted, percentCompleted)
-            innerCircleColor = typedArray.getColor(R.styleable.CircularProgressBar_innerCircleColor, innerCircleColor)
+        attrs?.let { attributes ->
+            val typedArray =
+                context.obtainStyledAttributes(attributes, R.styleable.CircularProgressBar)
+            textSize = typedArray.getDimensionPixelSize(
+                R.styleable.CircularProgressBar_mtextSize,
+                textSize
+            )
+            startAngel =
+                typedArray.getFloat(R.styleable.CircularProgressBar_mstartAngle, startAngel)
+            progressBarWidth = typedArray.getFloat(
+                R.styleable.CircularProgressBar_progressBarWidth,
+                progressBarWidth
+            )
+            percentCompleted = typedArray.getFloat(
+                R.styleable.CircularProgressBar_mpercentCompleted,
+                percentCompleted
+            )
+            innerCircleColor = typedArray.getColor(
+                R.styleable.CircularProgressBar_innerCircleColor,
+                innerCircleColor
+            )
             textColor = typedArray.getColor(R.styleable.CircularProgressBar_mtextColor, textColor)
-            progressCircleColor = typedArray.getColor(R.styleable.CircularProgressBar_progressCircleColor, progressCircleColor)
+            progressCircleColor = typedArray.getColor(
+                R.styleable.CircularProgressBar_progressCircleColor,
+                progressCircleColor
+            )
             typedArray.recycle()
         }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        var innerGap = 20f + (progressBarWidth/2)
-        innerRect = RectF(progressBarWidth+innerGap, progressBarWidth+innerGap, calculatedWidth.toFloat() - progressBarWidth-innerGap, calculatedHeight.toFloat()- progressBarWidth-innerGap)
 
-        progressRect = RectF(progressBarWidth, progressBarWidth, calculatedWidth.toFloat()-progressBarWidth, calculatedHeight.toFloat()-progressBarWidth)
+        innerRect.right = calculatedWidth.toFloat() - progressBarWidth - innerGap
+        innerRect.bottom = calculatedHeight.toFloat() - progressBarWidth - innerGap
 
-        var paint = Paint()
-        paint.isAntiAlias = true
-
+        progressRect.right = calculatedWidth.toFloat() - progressBarWidth
+        progressRect.bottom = calculatedHeight.toFloat() - progressBarWidth
 
         drawInnerCircle(canvas, paint)
-
         drawProgressCircle(canvas, paint)
 
-        if(!percentageCompletedText.isNullOrBlank()) {
+        if (!percentageCompletedText.isBlank()) {
             val centerX = (innerRect.left + innerRect.right) / 2
             val centerY = (innerRect.top + innerRect.bottom) / 2
             drawPercentageCompletedText(percentageCompletedText, paint, canvas, centerX, centerY)
@@ -106,12 +113,23 @@ class CircularProgressBar : View {
         canvas.drawArc(progressRect, startAngel, angle, false, paint)
     }
 
-    private inline fun drawPercentageCompletedText(str: String, paint: Paint, canvas: Canvas, centerX: Float, centerY: Float) {
+    private inline fun drawPercentageCompletedText(
+        str: String,
+        paint: Paint,
+        canvas: Canvas,
+        centerX: Float,
+        centerY: Float
+    ) {
         paint.textSize = textSize.toFloat()
         paint.color = textColor
         paint.style = Paint.Style.FILL
         paint.getTextBounds(str, 0, str.length, textBounds)
-        canvas.drawText(str, centerX - textBounds.width()/2, centerY + (textBounds.height()/2), paint)
+        canvas.drawText(
+            str,
+            centerX - textBounds.width() / 2,
+            centerY + (textBounds.height() / 2),
+            paint
+        )
     }
 
     fun setProgress(percentCompleted: Float) {
@@ -144,7 +162,10 @@ class CircularProgressBar : View {
         return result
     }
 
-    inner class CircleAngleAnimation(private val circle: CircularProgressBar, percentCompleted: Float) : Animation() {
+    inner class CircleAngleAnimation(
+        private val circle: CircularProgressBar,
+        percentCompleted: Float
+    ) : Animation() {
         private var oldAngle: Float = angle
         private var newAngle: Float = (360 * percentCompleted / 100)
 
